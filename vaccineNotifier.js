b7e3e38566e6374ab71cf1d6bfb9ccd4e14df76f
Aaish Sindwani <aaishsindwani@gmail.com>
2021-05-21 18:23:09 +0530

    district wise

--- vaccineNotifier.js
+++ vaccineNotifier.js
@@ -17,7 +17,9 @@ To close the app, run: pm2 stop vaccineNotifier.js && pm2 delete vaccineNotifier
 const PINCODE = process.env.PINCODE
 const FROMEMAIL = process.env.EMAIL
 const AGE = process.env.AGE
-const DISTRICT_EMAIL_MAP = { "aaishsindwani@gmail.com": [507] }
+const DOSE = process.env.DOSE
+const DISTRICT_EMAIL_MAP = { "aaishsindwani@gmail.com": [108] }
+const startDate = '2021-05-29'
 
 
 async function main(){
@@ -43,7 +45,8 @@ async function checkAvailability() {
 function getSlotsForDate(DATE) {
     for(let email in DISTRICT_EMAIL_MAP){
         console.log("Email: "+email)
-        
+        let userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36' + Math.random().toString(36).substring(1)
+        console.log(userAgent)
 	//let pincodes = PINCODE.split(',');
          DISTRICT_EMAIL_MAP[email].forEach(district => {
             let config = {
@@ -51,7 +54,7 @@ function getSlotsForDate(DATE) {
                 url: 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=' + district + '&date=' + DATE,
                 headers: {
                     'accept': 'application/json',
-                    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+                    'user-agent': userAgent
                 }
             };
 
@@ -59,9 +62,18 @@ function getSlotsForDate(DATE) {
                 .then(function (slots) {
                     let centers = slots.data.centers;
                     let validCenters = [];
+                    //console.log("dose= "+DOSE)
                     //console.log(centers[0])
+                    let validSlots={}
                     centers.forEach(function (centre, index) {
-                        let validSlots = centre.sessions.filter(slot => slot.min_age_limit <= AGE &&  slot.available_capacity > 0)
+                        if(DOSE==1){
+                            validSlots = centre.sessions.filter(slot => slot.min_age_limit <= AGE &&  slot.available_capacity > 0
+                                && slot.available_capacity_dose1 > 0)
+                        }
+                        else{
+                            validSlots = centre.sessions.filter(slot => slot.min_age_limit <= AGE &&  slot.available_capacity > 0
+                                && slot.available_capacity_dose2 > 0)
+                        }
                         centre.sessions = validSlots;
                         if(validSlots.length>0){
                             validCenters = validCenters.concat(centre);
@@ -93,11 +105,18 @@ async function notifyMe(validSlots, email){
 
 async function fetchNext2Days(){
     let dates = [];
-    let today = moment();
-    for(let i = 0 ; i < 2 ; i ++ ){
+    let today = "";
+    if(startDate != undefined){
+        today = moment(startDate);        
+    }
+    else{
+        today = moment();
+    }
+    for(let i = 0 ; i < 3 ; i ++ ){
         let dateString = today.format('DD-MM-YYYY')
         dates.push(dateString);
         today.add(1, 'day');
+        console.log("today is"+today)
     }
     return dates;
 }
